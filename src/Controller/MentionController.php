e7ce1c10fa41c208d24b604fa9d106f244508a68
Mauréaux Olivier <olivier.maureaux@ac-guadeloupe.fr>
2024-01-15 08:34:17 -0400

    Mise à jour du controlleur mention

--- src/Controller/MentionController.php
+++ src/Controller/MentionController.php
@@ -16,10 +16,53 @@ class MentionController extends AbstractController
     /**
      * @Route("/mention", name="mention")
      */
-    public function index(MonApplication $monApplication)
+    public function index()
     {
+        // Créer une instance du client HTTP Symfony
+        $client = HttpClient::create();
+
+        // Définir l'URL de l'API que vous souhaitez interroger
+        $url = $_SERVER["URLAPI"];
+
+        // Définir le token API de type Bearer
+        $token = $_SERVER["TOKENAPI"];
+
+        // Envoyer la requête à l'API avec le token dans l'en-tête Authorization
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                // 'Authorization' => 'Bearer ' . $token,
+                // 'auth_bearer' => $token,
+                'x-auth-token' => $token,
+                'Accept' => 'application/json', // Ajoutez d'autres en-têtes si nécessaire
+            ],
+        ]);
+
+        try {
+            // Récupérez le contenu de la réponse au format JSON
+            
+            $data = $response->toArray();
+            // dd($data[0]);
+            // Traitez les données de la réponse
+            // ...
+
+            // Retournez ou utilisez les données selon vos besoins
+            // return $data;
+        } 
+        catch (ExceptionInterface $e) {
+            // Gérez les erreurs de requête
+            // ...
+
+            // Retournez ou gérez les erreurs selon vos besoins
+            // return $e->getMessage();
+            // dd($e->getMessage());
+            return $this->render('erreur/erreur.html.twig', [
+                "module" =>'Mention',
+                "message" => $e->getMessage()
+            ]);
+        }
+
         return $this->render('mention/mention.html.twig', [
-            'monApplication' => $monApplication,
+            "data" =>$data
         ]);
     }
 }
