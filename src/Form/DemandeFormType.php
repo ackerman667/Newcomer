9465c981bf058503bf4f0c13acd864f81fbcef3d
ackerman667 <nbarbeu@gmail.com>
2024-07-02 08:45:03 -0400

    form

--- src/Form/DemandeFormType.php
+++ src/Form/DemandeFormType.php
@@ -6,9 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use App\Entity\Demandes;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -33,6 +34,7 @@ class DemandeFormType extends AbstractType
             ])
             ->add('date_de_naissance', BirthdayType::class, [
                 'label' => 'Date de naissance :',
+                'widget' => 'single_text',
                 'attr' => ['class' => 'form-control'],
                 
             ])
@@ -44,6 +46,117 @@ class DemandeFormType extends AbstractType
                 'label' => 'Choisissez un service :',
                 'choices' => $options['services'],
                 'required' => true,
+            ])
+            ->add('statut', ChoiceType::class, [
+                'label' => 'Statut :',
+                'choices' => [
+                    'Titulaire' => 'Titulaire',
+                    'Contractuel' => 'Contractuel',
+                    'Apprenti' => 'Apprenti',
+                    'Service Civique' => 'Service Civique',
+                    'Stagiaire' => 'Stagiaire',
+                ],
+                'placeholder' => 'Sélectionner un statut',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('missions', TextareaType::class, [
+                'label' => 'Mission(s) :',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('date_debut_contrat', DateType::class, [
+                'label' => 'Date de début de contrat :',
+                'widget' => 'single_text',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control contract-date',
+                    // 'style' => 'display: none;',  // Hidden by default
+                ],
+            ])
+            ->add('date_fin_contrat', DateType::class, [
+                'label' => 'Date de fin de contrat :',
+                'widget' => 'single_text',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control contract-date',
+                    // 'style' => 'display: none;',  // Hidden by default
+                ],
+            ])
+            ->add('telephone_bureau', TextType::class, [
+                'label' => 'N° de téléphone du bureau :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('messagerie_academique', ChoiceType::class, [
+                'label' => 'Besoin d’une adresse de messagerie académique :',
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'attr' => ['class' => 'form-check'],
+            ])
+            ->add('acces_dossiers', ChoiceType::class, [
+                'label' => 'Besoin d’accès à des dossiers partagés :',
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'attr' => ['class' => 'form-check'],
+            ])
+            ->add('liste_dossiers', TextareaType::class, [
+                'label' => 'Si oui, merci de les lister :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('imprimante_print_on_demand', ChoiceType::class, [
+                'label' => 'Besoin d’accéder à une imprimante autre que « PrintOnDemand » :',
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'attr' => ['class' => 'form-check'],
+            ])
+            ->add('liste_imprimantes', TextareaType::class, [
+                'label' => 'Si oui, merci de lister le ou les numéros des bureaux où se trouvent les imprimantes :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+                
+            ])
+            ->add('remplacement_nom', TextType::class, [
+                'label' => 'Nom :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('remplacement_prenom', TextType::class, [
+                'label' => 'Prénom :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('telephone_avant_service', TextType::class, [
+                'label' => 'Numéro de téléphone avant de quitter le service :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('parti_rectorat', ChoiceType::class, [
+                'label' => 'Parti du Rectorat :',
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'attr' => ['class' => 'form-check'],
+            ])
+            ->add('nouvelle_affectation_service', TextType::class, [
+                'label' => 'Si non, dans quel service est la nouvelle affectation :',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
             ]);
     }
     public function configureOptions(OptionsResolver $resolver)
