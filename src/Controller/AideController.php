45f7b2512a3067bb2c5943ab664c656c847278c3
Nicolas <Nicolas.Barbeu@ac-guadeloupe.fr>
2024-10-24 16:46:38 -0400

    correction

--- src/Controller/AideController.php
+++ src/Controller/AideController.php
@@ -14,10 +14,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AideController extends AbstractController
 {
    
-    #[Route('/aide', name: 'aide')]
+    #[Route('formulaireldap/aide', name: 'aide')]
     public function index(MonApplication $monApplication)
     {
         return $this->render('aide/index.html.twig', [
+            'monApplication' => $monApplication,
+        ]);
+    }
+
+
+    #[Route('/aide', name: 'aide_externe')]
+    public function index2(MonApplication $monApplication)
+    {
+        return $this->render('aide/index2.html.twig', [
             'monApplication' => $monApplication,
         ]);
     }
