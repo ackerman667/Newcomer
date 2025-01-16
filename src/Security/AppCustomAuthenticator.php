5d6f0e1445def7ea551402f69da60c792f43d736
ackerman667 <nbarbeu@gmail.com>
2025-01-16 09:40:39 -0400

    style

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -9,6 +9,10 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -17,11 +21,14 @@ use Symfony\Component\Security\Http\Util\TargetPathTrait;
 class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
+    private UserRepository $userRepository;
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator , UserRepository $userRepository)
     {
+        $this->userRepository = $userRepository;
+
     }
 
     public function authenticate(Request $request): Passport
@@ -30,6 +37,15 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         $email = $request->request->get('email', '');
         $password = $request->request->get('password', '');
         $csrfToken = $request->request->get('_csrf_token');
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+        if (!$user) {
+            throw new BadCredentialsException('Cet email n’existe pas.');
+        }
+
+        // Vérifiez le mot de passe
+        if (!password_verify($password, $user->getPassword())) {
+            throw new BadCredentialsException('Mot de passe incorrect.');
+        }
 
         return new Passport(
             new UserBadge($email),
@@ -53,4 +69,14 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
     }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $message = $exception->getMessage();
+        $request->getSession()->getFlashBag()->add('error', $message);
+    
+        return new RedirectResponse($this->getLoginUrl($request));
+    }
+
+   
 }
