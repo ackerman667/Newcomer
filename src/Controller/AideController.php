5dd2098dcb4ae54180b4cfd5eee6a629c8c40fac
ackerman667 <nbarbeu@gmail.com>
2025-03-14 11:54:10 -0400

    aide

--- src/Controller/AideController.php
+++ src/Controller/AideController.php
@@ -15,17 +15,22 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use App\Service\UserRoleChecker;
 use App\Security\UserInformation;
+use App\Service\SuperUserChecker;
 
 
 class AideController extends AbstractController
 {
     private $security;
     private $roleChecker;
-    public function __construct(Security $security, UserRoleChecker $roleChecker)
+    private $superUserChecker;
+    public function __construct(Security $security, UserRoleChecker $roleChecker,  SuperUserChecker $superUserChecker)
     {
         $this->security = $security;
         $this->roleChecker = $roleChecker;
 
+        $this->superUserChecker = $superUserChecker;
+
+        $this->isSuperUser = $this->superUserChecker->isSuperUser();
 
         $this->isValideur = $this->roleChecker->isUserValideur();
  
@@ -39,9 +44,11 @@ class AideController extends AbstractController
         $currentUser = $this->security->getUser();
         $uid = $currentUser->getUid();
         $isValideur = $this->roleChecker->isUserValideur();
+        $isSuperUser  =  $this->superUserChecker->isSuperUser();
         return $this->render('aide/index.html.twig', [
             'monApplication' => $monApplication,
             'isValideur' => $isValideur,
+            'isSuperUser' => $isSuperUser,
         ]);
     }
 
